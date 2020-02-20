bd358073dec3835e20e6f96fbc27913afa662f07
Inessa Jaghatspanyan <inessa.aasdevel@outlook.com>
2020-02-20 12:55:16 +0400

    Add project files.

---
+++ BlazorTest/Data/ItemMaster.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorTest.Data
+{
+    public class ItemMaster
+    {
+        public string ItemName { get; set; }
+        public int SaleCount { get; set; }
+    }
+}
---
+++ BlazorTest/Data/ItemMasterService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorTest.Data
+{
+    public class ItemMasterService
+    {
+        public Task<ItemMaster[]> GetItemMasters()
+        {
+            var rd = new Random();
+            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new ItemMaster
+            {
+                ItemName = "Item" + rd.Next(1, 100),
+                SaleCount = rd.Next(20, 100),
+            }).ToArray());
+        }
+    }
+}
